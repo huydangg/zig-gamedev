4a4b27b34a95794657347b5fdab5f5063782ddb7
huydangg <huyqc.dang@gmail.com>
2024-09-07 14:39:36 +0700

    zgui: add candlestick plot
    add candlestick plot based on implot_demo.cpp

--- libs/zgui/src/zplot.cpp
+++ libs/zgui/src/zplot.cpp
@@ -362,4 +362,124 @@ extern "C"
         ImPlot::PlotText(text, x, y, p, flags);
     }
 
+ZGUI_API int BinarySearch(const double *arr, int l, int r, double x) {
+  if (r >= l) {
+    int mid = l + (r - l) / 2;
+    if (arr[mid] == x)
+      return mid;
+    if (arr[mid] > x)
+      return BinarySearch(arr, l, mid - 1, x);
+    return BinarySearch(arr, mid + 1, r, x);
+  }
+  return -1;
+}
+
+ZGUI_API void zguiPlot_SetupAxisFormat(ImAxis axis, const char *fmt) {
+  ImPlot::SetupAxisFormat(axis, fmt);
+};
+
+// void SetupAxisScale(ImAxis idx, ImPlotScale scale)
+ZGUI_API void zguiPlot_SetupAxisScale(ImAxis idx, ImPlotScale scale) {
+  ImPlot::SetupAxisScale(idx, scale);
+};
+
+// // Sets an axis' limits constraints.
+// IMPLOT_API void SetupAxisLimitsConstraints(ImAxis axis, double v_min, double
+// v_max);
+ZGUI_API void zguiPlot_SetupAxisLimitsConstraints(ImAxis axis, double v_min,
+                                                  double v_max) {
+  ImPlot::SetupAxisLimitsConstraints(axis, v_min, v_max);
+}
+
+// // Sets an axis' zoom constraints.
+// IMPLOT_API void SetupAxisZoomConstraints(ImAxis axis, double z_min, double
+// z_max);
+ZGUI_API void zguiPlot_SetupAxisZoomConstraints(ImAxis axis, double z_min,
+                                                double z_max) {
+  ImPlot::SetupAxisZoomConstraints(axis, z_min, z_max);
+}
+ZGUI_API
+void zguiPlot_PlotCandleStick(const char *label_id, int count, bool tooltip,
+                              float width_percent, float bullCol[4],
+                              float bearCol[4], const void *_xs,
+                              const double *opens, const double *closes,
+                              const double *lows, const double *highs) {
+  const double *xs = (const double *)_xs;
+
+  // get ImGui window DrawList
+  ImDrawList *draw_list = ImPlot::GetPlotDrawList();
+  // calc real value width
+  double half_width =
+      count > 1 ? (xs[1] - xs[0]) * width_percent : width_percent;
+  // custom tool
+  if (ImPlot::IsPlotHovered() && tooltip) {
+    ImPlotPoint mouse = ImPlot::GetPlotMousePos();
+    mouse.x =
+        ImPlot::RoundTime(ImPlotTime::FromDouble(mouse.x), ImPlotTimeUnit_Day)
+            .ToDouble();
+    float tool_l = ImPlot::PlotToPixels(mouse.x - half_width * 1.5, mouse.y).x;
+    float tool_r = ImPlot::PlotToPixels(mouse.x + half_width * 1.5, mouse.y).x;
+    float tool_t = ImPlot::GetPlotPos().y;
+    float tool_b = tool_t + ImPlot::GetPlotSize().y;
+    ImPlot::PushPlotClipRect();
+    draw_list->AddRectFilled(ImVec2(tool_l, tool_t), ImVec2(tool_r, tool_b),
+                             IM_COL32(128, 128, 128, 64));
+    ImPlot::PopPlotClipRect();
+    // find mouse location index
+    int idx = BinarySearch(xs, 0, count - 1, mouse.x);
+    // render tool tip (won't be affected by plot clip rect)
+    if (idx != -1) {
+      ImGui::BeginTooltip();
+      char buff[32];
+      ImPlot::FormatDate(ImPlotTime::FromDouble(xs[idx]), buff, 32,
+                         ImPlotDateFmt_DayMoYr, ImPlot::GetStyle().UseISO8601);
+      ImGui::Text("Day:   %s", buff);
+      ImGui::Text("Open:  $%.2f", opens[idx]);
+      ImGui::Text("Close: $%.2f", closes[idx]);
+      ImGui::Text("Low:   $%.2f", lows[idx]);
+      ImGui::Text("High:  $%.2f", highs[idx]);
+      ImGui::EndTooltip();
+    }
+  }
+
+  // begin plot item
+  if (ImPlot::BeginItem(label_id)) {
+    // override legend icon color
+    ImPlot::GetCurrentItem()->Color = IM_COL32(64, 64, 64, 255);
+
+    ImPlotContext &gp = *GImPlot;
+    ImPlotPoint plot_start =
+        ImPlot::PixelsToPlot(gp.CurrentPlot->AxesRect.Min.x, 0);
+    ImPlotPoint plot_end =
+        ImPlot::PixelsToPlot(gp.CurrentPlot->AxesRect.Max.x, 0);
+    // fit data if requested
+    //
+    if (ImPlot::FitThisFrame()) {
+      for (int i = 0; i < count; ++i) {
+        if (xs[i] >= plot_start.x && xs[i] <= plot_end.x) {
+          ImPlot::FitPoint(ImPlotPoint(xs[i], lows[i]));
+          ImPlot::FitPoint(ImPlotPoint(xs[i], highs[i]));
+        }
+      }
+    }
+    // render data
+    for (int i = 0; i < count; ++i) {
+      if (xs[i] >= plot_start.x && xs[i] <= plot_end.x) {
+        ImVec2 open_pos = ImPlot::PlotToPixels(xs[i] - half_width, opens[i]);
+        ImVec2 close_pos = ImPlot::PlotToPixels(xs[i] + half_width, closes[i]);
+        ImVec2 low_pos = ImPlot::PlotToPixels(xs[i], lows[i]);
+        ImVec2 high_pos = ImPlot::PlotToPixels(xs[i], highs[i]);
+        // printf("open pos: %f, %f\n", open_pos.x, open_pos.y);
+        ImU32 color = ImGui::GetColorU32(
+            opens[i] > closes[i] ? (*(const ImVec4 *)&(bearCol[0]))
+                                 : (*(const ImVec4 *)&(bullCol[0])));
+        draw_list->AddLine(low_pos, high_pos, color);
+        draw_list->AddRectFilled(open_pos, close_pos, color);
+      }
+    }
+
+    // end plot item
+    ImPlot::EndItem();
+  }
+}
 } /* extern "C" */
